1d983a0c5934eed806be7f6bcbc5ec7f66ffe1fd
Antoni <antoninowalski@gmail.com>
2022-11-09 14:33:14 +0100

    Entities

---
+++ WebApplication11/Entities/RestaurantDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication11.Entities
+{
+    public class RestaurantDbContext: DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Address> addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
